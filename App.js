1ecbe385f83785fb1e97a5b58d4872f9af86a78e
Retno <retno@ITKD000006-MAC.local>
2018-04-17 21:59:42 +0700

    my first git

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import { Provider } from 'react-redux'
+import store from './src/store/Store'
+import Setup from './src/Setup'
+import Intro from './src/intro'
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Intro />
+      </Provider>
+    );
+  }
+}
+
+export default App
